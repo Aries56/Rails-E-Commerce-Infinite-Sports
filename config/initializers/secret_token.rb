1a89befdb28bb108669d1d2a2bd0a82bb38e306c
Michael Webb <mike_webb56@hotmail.com>
2013-11-21 16:46:33 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+InfiniteSportsStore::Application.config.secret_token = '11e6213897a01087f5a5de87c0c131a3f0de04eed11ff64bf0745d86d4121db0f817d791010fb25770e135a9e2c4db907f7e71dcc4f9db8dcbfb762126a53f2a'
