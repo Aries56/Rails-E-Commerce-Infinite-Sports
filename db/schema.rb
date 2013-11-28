c9823a55f0d382a4033ba3f39533bd20a731eaf8
Michael Webb <mike_webb56@hotmail.com>
2013-11-28 15:28:40 -0600

    Created 'About Us' and 'Contact Us' page. Associated the 'Page' model with ActiveAdmin to allow for easy editing

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131122191215) do
+ActiveRecord::Schema.define(:version => 20131128210336) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -76,6 +76,14 @@ ActiveRecord::Schema.define(:version => 20131122191215) do
     t.integer  "customer_id"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+  end
+
+  create_table "pages", :force => true do |t|
+    t.string   "name"
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "products", :force => true do |t|
